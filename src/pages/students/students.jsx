90aa3bb137da7a2dd7e4799654742d138215caa4
Mr. Awais <161300531+Awais68@users.noreply.github.com>
2025-01-09 00:48:45 +0500

    Data filters Added

--- src/pages/students/students.jsx
+++ src/pages/students/students.jsx
@@ -60,7 +60,81 @@ export default function Students() {
 
     return (
         <div>
-            Students Here
+            <div className="container mx-auto p-4" >
+                <h1 className="text-2xl font-bold">Students</h1>
+                <div>
+                    <select
+                        onChange={(e) => setFilter({ ...filter, course: e.target.value })}
+                        className="w-56 border p-2"
+                    >
+                        <option key={"all"} value={"all"}>
+                            Select Course
+                        </option>
+                        {courses.map((data) => (
+                            <option key={data._id} value={data._id}>
+                                {data.title}
+                            </option>
+                        ))}
+
+                    </select>
+                    <select
+                        onChange={(e) => setFilter({ ...filter, aboveAge: e.target.value })}
+                        className="w-56 border p-2"
+                    >
+                        <option key={"all"} value={"all"}>
+
+                            Above Age
+                        </option>
+                        {[...Array(100).keys()].map((data) => (
+
+                            <option key={data} value={data}>
+                                {data}
+                            </option>
+                        ))}
+                    </select>
+                    <select
+                        onChange={(e) => setFilter({ ...filter, belowAge: e.target.value })}
+                        className="w-56 border p-2"
+                    >
+                        <option key={"all"} value={"all"}>
+                            Below Age
+                        </option>
+                        {[...Array(100).keys()].map((data) => (
+                            <option key={data} value={data}>
+                                {data}
+                            </option>
+                        ))}
+                    </select>
+                    <select
+                        onChange={(e) => setFilter({ ...filter, city: e.target.value })}
+                        className="w-56 border p-2"
+                    >
+
+                        <option key={"all"} value={"all"}>
+                            Select City
+                        </option>
+
+                        {
+                            [
+                                "Lahore",
+                                "Islamabad",
+                                "Karachi",
+                                "Quetta",
+                                "Multan",
+                                "Rawalpindi",
+                                "Faisalabad",
+                            ]
+                                .map((data) => (
+                                    <option key={data} value={data}>
+                                        {data}
+                                    </option>
+                                ))
+                        }
+
+                    </select>
+
+                </div>
+            </div>
         </div>
     )
 }
